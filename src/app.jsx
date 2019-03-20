e0f594f86d062a64723a1d86e9471746eb3b288f
Jose Angel Herrera Jacobo <jose.herrera@fygsolutions.com>
2019-03-19 18:19:46 -0600

    Instalación de React Redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Alumnos from './components/alumnos/index.jsx'
+import { Provider } from 'react-redux'
 
 const app = document.getElementById('app')
 const HolaMundo = <h1>Hola Mundo1</h1>
